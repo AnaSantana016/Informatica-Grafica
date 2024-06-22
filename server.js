4ee8f2f849f99b1a1f95920d44c777da4c7cc9ed
Ana del Carmen Santana Ojeda <72494472+AnaSantana016@users.noreply.github.com>
2024-06-22 22:50:44 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index_S6.html');
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
